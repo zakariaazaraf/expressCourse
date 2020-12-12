b73360d0a65b739082b7ce2fe4c2d63615d3fd64
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-12 23:51:30 +0100

    install body-parser to destruct tha data, and use Post to bring data from the client

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const path = require('path') // path modules from nodejs
 
 const router = express.Router() // Use Configuration To Use This Method
 
+const bodyParser = require('body-parser') // 
+
 
 
 app.use(express.static('./staticFile'))
+app.use(bodyParser()) // Config body-parser package
+
 
 router.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/index.html'))
@@ -19,8 +23,15 @@ router.get('*', (req, res)=>{
     res.json('Wrong Route :( :(')
 })
  
+// To Get Data Properly Use body-parser which destruct the data and return the object
+router.post('/user', (req, res) =>{ 
+    // Get The Data Posted By The Clinet
+    console.log(req.body)
 
-app.use('/', router)
+    res.json({mes: ''})
+})
+
+app.use('/', router) // router Configuration
 
 
 app.listen(5000, () =>{
