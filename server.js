d0db26646aec74a8b41fa62abb1605c77926c276
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-12 20:13:26 +0100

    Send Static Files {html, css, js, ect} To The Server

--- server.js
+++ server.js
@@ -2,49 +2,26 @@ const express = require('express')
 
 const app = express(); // Install express application 
 
+const path = require('path') // path modules from nodejs
+
 const router = express.Router() // Use Configuration To Use This Method
 
-// Handel The Router
 
-// ============= First Methos ================
 
-/* app.get('/', (req, res) =>{
-    res.json('Is Works !!!')
+app.use(express.static('./staticFile'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get('/zakaria', (request, response) =>{
-    response.json('You\'re In Zakaria Route')
-})
-
-app.get('/azaraf', (req, res)=>{
-    res.json('You\'re In azaraf Profile !!')
-})
 
 app.get('*', (req, res)=>{
     res.json('Wrong Route :( :(')
 })
- */
+ 
 
 
-// ============== Second Methos ====================
 
- app.get('/', (req, res) =>{
-    res.json('Is Works !!!')
-})
-
-router.get('/zakaria', (request, response) =>{
-    response.json('You\'re In Zakaria Route')
-})
-
-router.get('/azaraf', (req, res)=>{
-    res.json('You\'re In azaraf Profile !!')
-})
-
-router.get('*', (req, res)=>{
-    res.json('Wrong Route :( :(')
-})
-
-app.use('/', router);
 
 app.listen(5000, () =>{
     console.log('Runinng The Server At Port:', 5000);
---
+++ staticFile/app.js
@@ -0,0 +1,1 @@
+console.log('Test express From Static File');
