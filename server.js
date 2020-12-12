205ab06cb7215e93c793a647a28d2e7c6ee7f77d
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-12 19:23:04 +0100

    test the two routing methods

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express(); // Install express application 
 
+const router = express.Router() // Use Configuration To Use This Method
+
 // Handel The Router
 
-app.get('/', (req, res) =>{
+// ============= First Methos ================
+
+/* app.get('/', (req, res) =>{
     res.json('Is Works !!!')
 })
 
@@ -12,6 +16,36 @@ app.get('/zakaria', (request, response) =>{
     response.json('You\'re In Zakaria Route')
 })
 
+app.get('/azaraf', (req, res)=>{
+    res.json('You\'re In azaraf Profile !!')
+})
+
+app.get('*', (req, res)=>{
+    res.json('Wrong Route :( :(')
+})
+ */
+
+
+// ============== Second Methos ====================
+
+ app.get('/', (req, res) =>{
+    res.json('Is Works !!!')
+})
+
+router.get('/zakaria', (request, response) =>{
+    response.json('You\'re In Zakaria Route')
+})
+
+router.get('/azaraf', (req, res)=>{
+    res.json('You\'re In azaraf Profile !!')
+})
+
+router.get('*', (req, res)=>{
+    res.json('Wrong Route :( :(')
+})
+
+app.use('/', router);
+
 app.listen(5000, () =>{
     console.log('Runinng The Server At Port:', 5000);
-})
+}) 
